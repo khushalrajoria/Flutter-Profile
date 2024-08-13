258b4f804e618c07691ee98bd25950fbb7e771fb
Khushal rajoria <rajoriakhushal@gmail.com>
2024-08-13 19:47:37 +0530

    tutorial commit

--- lib/pages/portfolio_page.dart
+++ lib/pages/portfolio_page.dart
@@ -187,180 +187,22 @@ class _PortfolioPageState extends State<PortfolioPage> {
   }
 }
 
+/*
+  So what i have done to integrate firebase is to first make a project in firebase with flutter option
+  then i have created a collection in the database and then i have used the following code to get
+  the data from the database and display it in the card widget
 
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+  the commands I have used are
+  firebase login
+  flutter pub global activate flutterfire_cli
+  flutterfire configure
+  
+  then I made changed main.dart 
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-// import '../main.dart';
-// import '../utils/AppColors.dart';
-// import '../utils/common_string.dart';
-// import '../utils/text_style.dart';
-
-// class PortfolioPage extends StatefulWidget {
-//   @override
-//   _PortfolioPageState createState() => _PortfolioPageState();
-// }
-
-// class _PortfolioPageState extends State<PortfolioPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     var width = MediaQuery.of(context).size.width;
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Portfolio'),
-//       ),
-//       body: StreamBuilder<QuerySnapshot>(
-//         stream: FirebaseFirestore.instance.collection('portfolio').snapshots(),
-//         builder: (context, snapshot) {
-//           if (!snapshot.hasData) {
-//             return Center(child: CircularProgressIndicator());
-//           }
-//           var projects = snapshot.data!.docs;
-//           return SingleChildScrollView(
-//             padding: EdgeInsets.only(left: 20, right: 20),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: <Widget>[
-//                 Text("Portfolio", style: mode ? darkheaderTextStyle : headerTextStyle),
-//                 Container(
-//                   child: Text(description, style: mode ? darkbodyTextStyle : bodyTextStyle),
-//                   margin: EdgeInsets.only(top: 20, bottom: 10),
-//                 ),
-//                 SizedBox(height: 3,),
-//                 Text("Click to see projects", style: mode ? darkbodyTextStyle : bodyTextStyle),
-//                 GridView.builder(
-//                   shrinkWrap: true,
-//                   physics: NeverScrollableScrollPhysics(),
-//                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                     crossAxisCount: 2,
-//                     crossAxisSpacing: 10,
-//                     mainAxisSpacing: 10,
-//                     childAspectRatio: 1,
-//                   ),
-//                   itemCount: projects.length,
-//                   itemBuilder: (context, index) {
-//                     var project = projects[index];
-//                     return itemWidget(
-//                       context,
-//                       project['Title'],
-//                       [Color.fromARGB(255, 193, 193, 193), Colors.white60],
-//                       [project['Title']],
-//                       [project['image']],
-//                       width,
-//                       project['description'],
-//                     );
-//                   },
-//                 ),
-//               ],
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
+  added this line to my main function and made it async
 
 
-// Widget itemWidget(BuildContext context, String title, List<Color> colors, List<String> titl, List<String> image, double width, String desc) {
-//   return Expanded(
-//     flex: 1,
-//     child: InkWell(
-//       onTap: () {
-//         showModalBottomSheet<void>(
-//           barrierColor: const Color.fromARGB(180, 158, 158, 158),
-//           shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(23),
-//           ),
-//           elevation: .5,
-//           context: context,
-//           builder: (BuildContext context) {
-//             return SingleChildScrollView(
-//               child: Container(
-//                 height: 720,
-//                 color: mode ? backgrounddark : backgroundLight,
-//                 width: width,
-//                 child: Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     SizedBox(height: 20),
-//                     Container(
-//                       height: 250,
-//                       child: ListView.builder(
-//                         scrollDirection: Axis.horizontal,
-//                         itemCount: titl.length,
-//                         itemBuilder: (BuildContext context, int index) {
-//                           return Column(
-//                             children: [
-//                               Stack(
-//                                 children: [
-//                                   Container(
-//                                     height: 200,
-//                                     width: width,
-//                                     alignment: Alignment.center,
-//                                     child: Image.network(image[index], fit: BoxFit.contain),
-//                                   ),
-//                                   Positioned(
-//                                     top: 5,
-//                                     right: 5,
-//                                     child: Container(
-//                                       height: 20,
-//                                       width: 40,
-//                                       decoration: BoxDecoration(
-//                                         color: Color.fromARGB(123, 0, 0, 0),
-//                                         borderRadius: BorderRadius.circular(30),
-//                                       ),
-//                                       child: Center(
-//                                         child: Text(
-//                                           "${index + 1}/${titl.length}",
-//                                           style: TextStyle(color: Colors.white),
-//                                         ),
-//                                       ),
-//                                     ),
-//                                   ),
-//                                 ],
-//                               ),
-//                               SizedBox(height: 10),
-//                               SizedBox(
-//                                 width: width,
-//                                 child: Center(
-//                                   child: Text(titl[index], style: mode ? darksubHeaderTextStyle : subHeaderTextStyle),
-//                                 ),
-//                               ),
-//                             ],
-//                           );
-//                         },
-//                       ),
-//                     ),
-//                     Padding(
-//                       padding: const EdgeInsets.only(left: 10.0, top: 5),
-//                       child: Text(desc, style: mode ? darkbodyTextStyle : bodyTextStyle),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             );
-//           },
-//         );
-//       },
-//       child: Container(
-//         margin: EdgeInsets.all(3),
-//         decoration: BoxDecoration(
-//           borderRadius: BorderRadius.circular(12),
-//           gradient: LinearGradient(
-//             colors: colors,
-//             begin: Alignment.topLeft,
-//             end: Alignment.bottomRight,
-//           ),
-//         ),
-//         height: 100,
-//         child: Center(
-//           child: Text(
-//             title,
-//             style: mode ? darkbodyTextStyle : bodyTextStyle,
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
+  then I used future builder/ stream builder to fetch and show the data in my card
+ */
