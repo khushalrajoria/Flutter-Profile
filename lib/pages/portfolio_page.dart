d07728f24bacf6359b5b66625891b072dd09f503
Khushal rajoria <rajoriakhushal@gmail.com>
2024-08-14 01:27:58 +0530

    minor changes

--- lib/pages/portfolio_page.dart
+++ lib/pages/portfolio_page.dart
@@ -181,7 +181,7 @@ class _PortfolioPageState extends State<PortfolioPage> {
         ),
         height: 100,
         alignment: Alignment.center,
-        child: Text(title, style: mode ? darkbodyTextStyle : bodyTextStyle),
+        child: Text(title, style: mode ? darkbodyTextStyle.copyWith(fontWeight: FontWeight.w800,color: Colors.white) : bodyTextStyle.copyWith(fontWeight: FontWeight.w800)),
       ),
     );
   }
--- lib/utils/text_style.dart
+++ lib/utils/text_style.dart
@@ -8,5 +8,5 @@ TextStyle subHeaderTextStyle =
     TextStyle(fontSize: 16, color: Colors.grey[700]);
 TextStyle darksubHeaderTextStyle =
     const TextStyle(fontSize: 16, color: Color.fromARGB(255, 255, 255, 255));
-TextStyle bodyTextStyle = const TextStyle(fontSize: 14, color: Color.fromARGB(255, 136, 134, 134),fontWeight: FontWeight.w700);
-TextStyle darkbodyTextStyle = const TextStyle(fontSize: 14, color: Color.fromARGB(255, 255, 255, 255),fontWeight: FontWeight.w700);
+TextStyle bodyTextStyle = const TextStyle(fontSize: 14, color: Color.fromARGB(255, 136, 134, 134));
+TextStyle darkbodyTextStyle = const TextStyle(fontSize: 14, color: Color.fromARGB(255, 255, 255, 255),);
